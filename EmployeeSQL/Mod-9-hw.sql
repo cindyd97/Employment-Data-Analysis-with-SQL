97bb6d89b2ef9db92b3945f12fa6dd185c9b95e6
cindawwgg <cindy.duong74@gmail.com>
2024-12-19 18:31:03 -0800

    working

--- EmployeeSQL/Mod-9-hw.sql
+++ EmployeeSQL/Mod-9-hw.sql
@@ -86,6 +86,13 @@ WHERE
 
 -- 8. List the frequency counts, in descending order, of all the employee last names
 --(that is, how many employees share each last name).
-
-
+SELECT 
+    last_name,
+    COUNT(*) AS frequency_count
+FROM 
+    employees
+GROUP BY 
+    last_name
+ORDER BY 
+    frequency_count DESC;
 	
